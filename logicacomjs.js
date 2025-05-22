9ad8203b074c928f7db77d1d07dd52f5e892d365
ghost1black <odeiomatematica1212@gmail.com>
2025-05-22 11:15:55 -0300

    Aqui eu vi let, consol, e fiz uma calculadora

---
+++ logicacomjs.js
@@ -0,0 +1,23 @@
+/*var x=32;
+var y=13;
+let som= x+y
+console.log("a soma é:" +som)
+console.log("---------------------------------------")
+var t=21;
+var f=5;
+let mult= t*f
+console.log("a multiplição é:" +mult)
+console.log("---------------------------------------")
+var x=87;
+var y=32;
+let div= x/y
+console.log("a divisão é:" +div)
+console.log("---------------------------------------")
+var x=87;
+var y=32;
+let sub= x-y
+console.log("a sulbtração é:" +sub)*/
+let nome= "vitoria";
+let salario= 750/12;
+let mesestrab= salario *8
+console.log (nome+ " seu salario final é:" +salario) 
