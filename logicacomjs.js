4fb3b54e6ecd3743be9ec4cd365bd643f66fc135
ghost1black <odeiomatematica1212@gmail.com>
2025-06-03 11:34:17 -0300

    novo eu

--- logicacomjs.js
+++ logicacomjs.js
@@ -16,8 +16,48 @@ console.log("---------------------------------------")
 var x=87;
 var y=32;
 let sub= x-y
-console.log("a sulbtração é:" +sub)*/
+console.log("a sulbtração é:" +sub)
 let nome= "vitoria";
 let salario= 750/12;
 let mesestrab= salario *8
 console.log (nome+ " seu salario final é:" +salario) 
+salario = 500
+quinze = 15/100*salario+salario
+dez = 10/100*salario+salario
+cinco = 5/100*salario+salario*
+let idade = prompt("digite aqui sua idade")
+if(idade<12){
+    alert("Assiste conteúdo infantil")
+}
+else if (idade=12 && idade <=17)
+alert("Assista contúdo teen")
+
+else if (idade>=28 && idade>60){
+   alert("Assista contúdo adulto")
+}
+else{
+    alert ("Assista contúdo sénioe")
+}
+ let ponto = prompt("digite aqui a sua ponto")
+ if (Ponto<=100){
+    alert("Seu Nivel é Novato!! Bónus de +100EXP")
+ }
+else if(ponto>=1001 && ponto<=500){
+     alert("Seu Nivel é Aptendiz!! Bónus de +500XP")
+}
+ else if(ponto>=5001 && ponto>=10000)
+  alert("Seu NIvel é Expert!! Bónus de +1000XP")
+
+    else{
+      alert("Seu NIvel é mestre!! Bónus de +2000XP")
+    }*/
+   let  valorbasico = 25;
+   let valorpremium = 40;
+   let valorultra = 60;
+   let tipassinatura = prompt("digite a sua assinatura:")
+   let quantitelas = prompt ("digite a quatidade de tela que deseja:")
+    if(tipoassinanatura == "básico"){
+        if(quantitelas >=1 && quantitelas <=2){
+            
+        }
+            alert("Seu plano teré o valor de "+valorbasico+ "R$")
